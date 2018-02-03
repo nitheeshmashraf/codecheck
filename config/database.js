558b640443b0983748eb13f458953e6d01190384
Nitheesh <nitheeshmashraf@gmail.com>
2018-02-03 12:36:34 +0400

    public heroku push

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database:'mongodb://Nit:nitz123@ds129344.mlab.com:29344/mytasklist_nithz',
+  database:'mongodb://sashdev:sadev@ds121238.mlab.com:21238/sashdevdb',
 	secret:'yoursecret'
   // database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
   // //database: 'mongodb://localhost:27017/meanauth',    //dev
